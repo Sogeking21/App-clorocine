0a1cb1840306f18424e62f7df957c4a47bfb609b
vitorhs <vitorhugosleite@gmail.com>
2021-02-06 01:00:12 -0300

    Cadastro de filmes p1

--- cadastrar.php
+++ cadastrar.php
@@ -7,7 +7,7 @@
     <div class="nav-wrapper">
 
       <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="index.php">Galeira</a></li>
+        <li><a href="galeria.php">Galeira</a></li>
         <li><a href="cadastrar.php">Cadastar</a></li>
       </ul>
     </div>
@@ -20,7 +20,9 @@
     </div>
   </nav>
   <!--Formulario-->
+  
   <div class="row">
+  <form method="POST" action="inserirfilme.php">
     <div class="col s6 offset-s3 ">
         <div class="card ">
             <div class="card-content white-text">
@@ -28,25 +30,24 @@
                 <!-- input titulo-->
                   <div class="row">
                     <div class="input-field col s6">
-                      <input  id="titulo" type="text" class="validate" require>
+                      <input  name="titulo" id="titulo" type="text" class="validate"  require>
                       <label for="titulo">titulo do filme</label>
                     </div>
                   </div>
               <!-- input sinopse-->
               <div class="row">
-                <form class="col s12">
-                  <div class="row">
-                    <div class="input-field col s12">
-                      <textarea id="sinopse" class="materialize-textarea"></textarea>
-                      <label for="sinopse">Sinopse</label>
-                    </div>
+                
+                <div class="row">
+                  <div class="input-field col s12">
+                    <textarea  name="sinopse" id="sinopse"class="materialize-textarea"></textarea>
+                    <label for="sinopse">Sinopse</label>
                   </div>
-                </form>
+                </div>
               </div>
               <!-- input nota-->
               <div class="row">
                     <div class="input-field col s1">
-                      <input  id="nota" type="number" step=".1" min="0" max="10" class="validate" require>
+                      <input  name="nota" id="nota" type="number" step=".1" min="0" max="10" class="validate" require>
                       <label for="nota">Nota</label>
                     </div>
                   </div>
@@ -54,20 +55,22 @@
                   <div class="file-field input-field">
                     <div class="btn">
                       <span>Capa</span>
-                      <input type="file">
+                      <input type="file" >
                     </div>
                     <div class="file-path-wrapper">
-                      <input class="file-path validate" type="text">
+                      <input class="file-path validate" type="text" name="poster">
                     </div>
                   </div>
                   <!-- botao cancelar e comfirmar-->
             <div class="card-action">
                 <a  class="btn" href="index.php">Cancelar</a>
-                <a href="#" class="waves-effect waves-light btn">Comfirmar</a>
+                <button type="submit" class="waves-effect waves-light btn">Confirmar</button>
             </div>
         </div>
     </div>
   </div>
+</form>
+</div>
 
 </body>
 </html>
