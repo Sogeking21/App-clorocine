f357f3f3afb61f0bb4244c12d526dd844c05fc0b
Sogeking21 <vitorhugosleite@gmail.com>
2021-12-18 10:37:45 -0300

    Heroku connection

--- repository/conexao.php
+++ repository/conexao.php
@@ -1,7 +1,19 @@
 <?php
 
-class conexao{
-    public  static function criar():PDO{
-        return new PDO ("sqlite:db/filmes.db");
+class conexao
+{
+    public  static function criar(): PDO
+    {
+        $env = parse_ini_file('.env');
+        $databasetype = $env["databasetype"];
+        $database = $env["database"];
+        $server = $env["server"];
+        $user = $env["user"];
+        $pass = $env["pass"];
+
+        if ($databasetype === "mysql") {
+            $database = "host=$server;dbname=$database";
+        }
+        return new PDO("$databasetype:$database", $user, $pass);
     }
 }
